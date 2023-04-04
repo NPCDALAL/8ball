3628df79e20ac5e7c80970316255d4c852459d11
NPCDALAL <124927029+NPCDALAL@users.noreply.github.com>
2023-04-04 13:26:48 -0600

    s

---
+++ main.js
@@ -0,0 +1,10 @@
+// Magic 8 Ball by Azer
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnclicked);
+
+function btnClicked() {
+    // Determine 8 ball response
+    let character = document.getElementById()
+    
+}
