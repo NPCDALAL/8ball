77a4835343f2f93b2bbc190b7195337139a7c0c1
NPCDALAL <a.ahmed134@share.epsb.ca>
2023-04-07 21:22:50 -0600

    s

--- main.js
+++ main.js
@@ -3,13 +3,12 @@
 // HTML Variables
 let response1 = document.getElementById("output");
 // Button Event Listener
-document.getElementById("btn").addEventListener("click", btnClicked);
+document.getElementById("img").addEventListener("click", imgClicked);
 
-function btnClicked() {
-    let character = document.getElementById()
+function imgClicked() {
   // Determine 8 ball response
   let randNum = Math.random();
-    console.log(randNum);
+  console.log(randNum);
   // 8 ball response
   if (randNum < 0.2) {
     output.innerHTML = "<p>Without a doubt.</p>";
@@ -19,6 +18,12 @@ function btnClicked() {
     output.innerHTML = "<p>Concentrate and ask again.</p>";
   } else if (randNum < 0.8) {
     output.innerHTML = "<p>Don't count on it.</p>";
-  } else (randNum < 1) 
+  } else {
     output.innerHTML = "<p>Outlook not so good.</p>";
+  }
+  let character = document.getElementById("search-in").value.toLowercase();
+  if (character === "does a magic 8 ball actually work?") {
+    output.innerHTML = "<p>How dare you doubt me!</p>";
+  } else if (character === 0) {
+  }
 }
