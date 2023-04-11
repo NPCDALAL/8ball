dfa2f78358f0ef6758668a46dad4e15411489169
NPCDALAL <a.ahmed134@share.epsb.ca>
2023-04-11 06:23:06 -0600

    s

--- main.js
+++ main.js
@@ -6,9 +6,19 @@ let output = document.getElementById("output");
 document.getElementById("img").addEventListener("click", imgClicked);
 
 function imgClicked() {
+  // INPUT
+  let question = document.getElementById("question-in").value.toLowerCase();
   let randNum = Math.random();
-  console.log(randNum);
-  if (randNum < 0.2) {
+  // If statements
+  if (question === "does a magic 8 ball actually work?") {
+    output.innerHTML = "<p>How dare you doubt me!</p>";
+  } else if (question === "is java script awesome?") {
+    output.innerHTML = "<p>Of course!</p>";
+  } else if (question === "is computer science cool?") {
+    output.innerHTML = "<p>Obviously, yes</p>";
+  } else if (question.length === 0) {
+    output.innerHTML = "Please ask a question...";
+  } else if (randNum < 0.2) {
     output.innerHTML = "<p>Without a doubt.</p>";
   } else if (randNum < 0.4) {
     output.innerHTML = "<p>As I see it, yes.</p>";
@@ -16,18 +26,5 @@ function imgClicked() {
     output.innerHTML = "<p>Concentrate and ask again.</p>";
   } else if (randNum < 0.8) {
     output.innerHTML = "<p>Don't count on it.</p>";
-  } else {
-    output.innerHTML = "<p>Outlook not so good.</p>";
-  }
-  let search = document.getElementById("search-in").value.toLowercase();
-  if (search === "does a magic 8 ball actually work?") {
-    output.innerHTML = "<p>How dare you doubt me!</p>";
-  } else if (search === "is java script awesome?") {
-    output.innerHTML = "<p>Of course!</p>";
-  } else if (character === "Is Computer Science cool?") {
-    output.innerHTML = "<p>Obviously, yes</p>";
-  } else search.length === 0;
-  {
-    output.innerHTML = "Please ask a question...";
-  }
+  } else output.innerHTML = "<p>Outlook not so good.</p>";
 }
