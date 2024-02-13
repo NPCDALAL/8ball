41dbbd76fccedb869f435a968b15e56b5bba2418
NPCDALAL <a.ahmed134@share.epsb.ca>
2024-02-13 01:08:49 -0700

    dsa

--- main.js
+++ main.js
@@ -8,23 +8,38 @@ document.getElementById("img").addEventListener("click", imgClicked);
 function imgClicked() {
   // INPUT
   let question = document.getElementById("question-in").value.toLowerCase();
-  let randNum = Math.random();
+    // Process
+    getResponse(question);
+    // Output
+    output.innerHTML = response;
+  }
+
+  function getResponse(question) {
   // If statements
-  if (question === "does a magic 8 ball actually work?") {
-    output.innerHTML = "<p>How dare you doubt me!</p>";
+  if (question.length === 0){
+    response = "Please ask a question...";
+  } else if (question === "does a magic 8 ball actually work?") {
+    response = "<p>How dare you doubt me!</p>";
   } else if (question === "is java script awesome?") {
-    output.innerHTML = "<p>Of course!</p>";
+    response = "<p>Of course!</p>";
   } else if (question === "is computer science cool?") {
-    output.innerHTML = "<p>Obviously, yes</p>";
-  } else if (question.length === 0) {
-    output.innerHTML = "Please ask a question...";
-  } else if (randNum < 0.2) {
-    output.innerHTML = "<p>Without a doubt.</p>";
-  } else if (randNum < 0.4) {
-    output.innerHTML = "<p>As I see it, yes.</p>";
-  } else if (randNum < 0.6) {
-    output.innerHTML = "<p>Concentrate and ask again.</p>";
-  } else if (randNum < 0.8) {
-    output.innerHTML = "<p>Don't count on it.</p>";
-  } else output.innerHTML = "<p>Outlook not so good.</p>";
+    response = "<p>Obviously, yes</p>";
+  } else {
+
+  }
 }
+  function randomResponse() {
+    let randNum = Math.random();
+
+    if (randNum < 0.2) {
+      response = "<p>Without a doubt.</p>";
+    } else if (randNum < 0.4) {
+      response = "<p>As I see it, yes.</p>";
+    } else if (randNum < 0.6) {
+      response = "<p>Concentrate and ask again.</p>";
+    } else if (randNum < 0.8) {
+      response = "<p>Don't count on it.</p>";
+    } else { response = "<p>Outlook not so good.</p>";
+  }
+return response;
+  }
