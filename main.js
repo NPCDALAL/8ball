dc4419da305e8fe1652666a0b9724de922739cbe
NPCDALAL <a.ahmed134@share.epsb.ca>
2024-02-26 20:45:17 -0700

    dsad

--- main.js
+++ main.js
@@ -8,22 +8,20 @@ document.getElementById("img").addEventListener("click", imgClicked);
 function imgClicked() {
   // INPUT
   let question = document.getElementById("question-in").value.toLowerCase();
-    // Process
-    getResponse(question);
-    // Output
-    output.innerHTML = response;
+    // OUTPUT
+    output.innerHTML = getResponse(question);
   }
 
   function getResponse(question) {
   // If statements
-  if (question.length === 0){
-    response = "Please ask a question...";
+  if (question === ""){
+    return"Please ask a question...";
   } else if (question === "does a magic 8 ball actually work?") {
-    response = "<p>How dare you doubt me!</p>";
+    return"<p>How dare you doubt me!</p>";
   } else if (question === "is java script awesome?") {
-    response = "<p>Of course!</p>";
+    return"<p>Of course!</p>";
   } else if (question === "is computer science cool?") {
-    response = "<p>Obviously, yes</p>";
+    return"<p>Obviously, yes</p>";
   } else {
 
   }
@@ -32,14 +30,13 @@ function imgClicked() {
     let randNum = Math.random();
 
     if (randNum < 0.2) {
-      response = "<p>Without a doubt.</p>";
+      return"<p>Without a doubt.</p>";
     } else if (randNum < 0.4) {
-      response = "<p>As I see it, yes.</p>";
+      return"<p>As I see it, yes.</p>";
     } else if (randNum < 0.6) {
-      response = "<p>Concentrate and ask again.</p>";
+      return"<p>Concentrate and ask again.</p>";
     } else if (randNum < 0.8) {
-      response = "<p>Don't count on it.</p>";
-    } else { response = "<p>Outlook not so good.</p>";
+      return"<p>Don't count on it.</p>";
+    } else { return"<p>Outlook not so good.</p>";
   }
-return response;
   }
